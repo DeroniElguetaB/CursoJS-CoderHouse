19f46889e6097b883616bd556e76b5384cfa1e9a
Deroni Elgueta <deroniaeb@gmail.com>
2022-07-04 00:52:01 -0300

    Proyecto MANGOS 3D E-Commerce P5

--- index.js
+++ index.js
@@ -100,87 +100,108 @@ while (question != "si" && question != "Si"){
 // -------USUARIOS RESTANTES - VISTA DE USUARIO-------------------------------------------------
         else {
             alert(`Hola ${usuario}! que deseas hacer?`)
-            let opcion = parseInt(prompt("1. Ver catalogo de productos. \n2. Realizar una compra de producto. \n3. Ingresar ESC para salir."))    
-//---------------- OPCION 1 VER CATALOGO DE PRODUCTOS ----------------------------------------------   
-                if (opcion == 1 || opcion == "1.") {
-                    productosDisponibles()
-                    alert("- Soporte patente moto 150cc - ($800) \n- Carcasas iphone/android - ($1500) \n- Porta-lampara ironman - ($1500) \n- Tapas maquinas Barber&Style = ($600) \n\nEncarga tus diseños unicos y personalizados! ")
-                }
-//---------------- OPCION 2 REALIZAR COMPRA DE PRODUCTOS ----------------------------------------------    
-                else if (opcion == 2 || opcion == "2.") {
-                    seleccionusuario = parseInt(prompt("(1) - Soporte patente moto 150cc - ($800) \n(2) - Carcasas iphone/android - ($1500) \n(3) - Porta-lampara ironman - ($1500) \n(4) - Tapas maquinas Barber&Style - ($600) \n \nSeleccione su producto con su numero correspondiente"))
-                    let carritoCompra = [{nombre:"alexis", precioVenta: 500},{}]
 
-                    class ItemsCarrito {
-                        constructor (nombre, precioVenta, cantidad){
-                            this.nombre = nombre.toUpperCase()
-                            this.precioVenta = precioVenta
-                            this.cantidad = cantidad
+            let opcion
+            
+            while((opcion != 1)&&(opcion != 2)&&(opcion != 3)){
+                opcion = parseInt(prompt("1. Ver catalogo de productos. \n2. Realizar una compra de producto. \n3. Salir del menu."))    
+//------------------ OPCION 1 VER CATALOGO DE PRODUCTOS ----------------------------------------------   
+                    if (opcion === 1) {
+                        productosDisponibles()
+                        alert("- Soporte patente moto 150cc - ($800) \n- Carcasas iphone/android - ($1500) \n- Porta-lampara ironman - ($1500) \n- Tapas maquinas Barber&Style = ($600) \n\nEncarga tus diseños unicos y personalizados! ")
+                    }
+//------------------ OPCION 2 REALIZAR COMPRA DE PRODUCTOS ----------------------------------------------    
+                    else if (opcion === 2) {
+                        seleccionusuario = parseInt(prompt("(1) - Soporte patente moto 150cc - ($800) \n(2) - Carcasas iphone/android - ($1500) \n(3) - Porta-lampara ironman - ($1500) \n(4) - Tapas maquinas Barber&Style - ($600) \n \nSeleccione su producto con su numero correspondiente"))
+                        let carritoCompra = []
+
+                        class ItemsCarrito {
+                            constructor (nombre, precioVenta, cantidad){
+                                this.nombre = nombre.toUpperCase()
+                                this.precioVenta = precioVenta
+                                this.cantidad = cantidad
+                            }
+                        }
+                        function mostrarItems (carritoCompra){
+                            for(const ItemsCarrito of carritoCompra){
+                                console.log(ItemsCarrito)
+                                console.log(ItemsCarrito.nombre)
+                            }
+                        }
+                        switch(seleccionusuario){
+//-------------------------- OPCION 2.1 - COMPRA ITEM 1 -----------------------------------------------------                       
+                            case 1:
+                                function agregarItem1 (){
+                                    let items1 = new ItemsCarrito ("Soporte patente moto 150cc", 800, 5)
+                                    carritoCompra.push(items1)
+                                    return carritoCompra
+                                }
+                                function main1(){
+                                    let productoAgregado1 = agregarItem1 ()
+                                    mostrarItems(productoAgregado1)
+                                }
+                                main1()
+                                let compra1 = prompt("Añadio Soporte patente moto 150cc - ($800) al carrito de compra \nDesea algo mas? \n\n Ingrese si/no")
+                                break
+//-------------------------- OPCION 2.2 - COMPRA ITEM 2 -----------------------------------------------------                       
+                            case 2:
+                                function agregarItem2 (){
+                                    let items2 = new ItemsCarrito ("Carcasa Iphone 12", 1500, 12)
+                                    carritoCompra.push(items2)
+                                    return carritoCompra
+                                }
+                                function main2(){
+                                    let productoAgregado2 = agregarItem2 ()
+                                    mostrarItems(productoAgregado2)
+                                }
+                                main2()
+                                let compra2 = prompt("Añadio Carcasa iphone/android - ($1500) al carrito de compra \nDesea algo mas? \n\n Ingrese si/no")
+                                break
+//-------------------------- OPCION 2.3 - COMPRA ITEM 3 -----------------------------------------------------                       
+                            case 3:
+                                function agregarItem3 (){
+                                    let items3 = new ItemsCarrito ("Porta-lampara ironman", 1500, 1)
+                                    carritoCompra.push(items3)
+                                    return carritoCompra
+                                }
+                                function main3(){
+                                    let productoAgregado3 = agregarItem3 ()
+                                    mostrarItems(productoAgregado3)
+                                }
+                                main3()
+                                let compra3 = prompt("Añadio Porta-lampara ironman - ($1500) al carrito de compra \nDesea algo mas? \n\n Ingrese si/no")
+                                break
+//-------------------------- OPCION 2.4 - COMPRA ITEM 4 -----------------------------------------------------                       
+                            case 4:
+                                function agregarItem4 (){
+                                    let items4 = new ItemsCarrito ("Tapas maquinas Barber&Style", 600, 4)
+                                    carritoCompra.push(items4)
+                                    return carritoCompra
+                                }
+                                function main4(){
+                                    let productoAgregado4 = agregarItem4 ()
+                                    mostrarItems(productoAgregado4)
+                                }
+                                main4()
+                                let compra4 = prompt("Añadio Tapas maquinas Barber&Style - ($600) al carrito de compra \nDesea algo mas? \n\n Ingrese si/no")
+                                break
+//-------------------------- OPCION COMANDO INVALIDO -----------------------------------------------------                       
+                            default:
+                                invalido()
+                                break
                         }
                     }
-                    function mostrarItems (carritoCompra){
-                        for(const ItemsCarrito of carritoCompra){
-                            console.log(ItemsCarrito)
-                            console.log(ItemsCarrito.nombre)
-                        }
+//------------------ OPCION 3 SALIR  -----------------------------------------------------------------    
+                    else if(opcion === 3){
+                        alert("Gracias por contar con MANGOS 3D, vuelva pronto!")
                     }
-                    switch(seleccionusuario){
-//------------------------OPCION 1 - COMPRA ITEM 1 -----------------------------------------------------                       
-                        case 1:
-                            function agregarItem1 (){
-                                let compra =prompt("Añadio Soporte patente moto 150cc - ($800) al carrito de compra \nDesea algo mas? \n\n Ingrese si/no")
-                                let items1 = new carritoCompra ("Soporte patente moto 150cc", 800, 5)
-                                carritoCompra.push(items1)
-                                return carritoCompra
-                            }
-                            function main(){
-                                let productoAgregado = agregarItem1 ()
-                                mostrarItems(productoAgregado)
-                            }
-                            main()
-//------------------------------------OPCION "SI" DESEA AGREGAR ALGO MAS AL CARRITO -------------------------                     
-                                if(opcion1 === "si"){
-                                    function agregarMas (){
-                                        let item1 = parseInt(prompt("(1) - Soporte patente moto 150cc - ($800) \n(2) - Carcasas iphone/android - ($1500) \n(3) - Porta-lampara ironman - ($1500) \n(4) - Tapas maquinas Barber&Style = ($600) \n \nSeleccione su producto con su numero correspondiente"))
-                                    }
-                                    agregarMas()
-                                }
-//------------------------------------OPCION "NO" DESEA AGREGAR ALGO MAS AL CARRITO -------------------------
-                                else{
-                                alert("Muchas gracias por comprar con Mangos 3D!")
-                                }
-                            break
-//------------------------OPCION 2 - COMPRA ITEM 2 -----------------------------------------------------                       
-                        case 2:
-                            let opcion2 = (prompt("Añadio Carcasa iphone/android - ($1500) al carrito de compra \nDesea algo mas?"))
-                            alert("Muchas gracias por comprar con Mangos 3D!")
-                            break
-//-----------------------OPCION 3 - COMPRA ITEM 3 -----------------------------------------------------                       
-                        case 3:
-                            let opcion3 = (prompt("Añadio Porta-lampara ironman - ($1500) al carrito de compra \nDesea algo mas?"))
-                            alert("Muchas gracias por comprar con Mangos 3D!")
-                            break
-//------------------------OPCION 4 - COMPRA ITEM 4 -----------------------------------------------------                       
-                        case 4:
-                            let opcion4 = (prompt("Añadio Tapa maquinas Barber&Style = ($600) al carrito de compra \nDesea algo mas?"))
-                            alert("Muchas gracias por comprar con Mangos 3D!")
-                            break
-//------------------------OPCION COMANDO INVALIDO -----------------------------------------------------                       
-                        default:
-                            invalido()
-                            break
+//------------------ OPCION COMANDO INVALIDO  ---------------------------------------------------------    
+                    else {
+                        invalido()
                     }
-                }
-//---------------- OPCION 3 SALIR  -----------------------------------------------------------------    
-                else if (opcion === "ESC") {
-                    alert("Gracias, esperemos que vuelva pronto!")
-                }
-//---------------- OPCION COMANDO INVALIDO  ---------------------------------------------------------    
-                // else {
-                //     invalido()
-                // }
             }
         }
+    }
 // PREGUNTA SI ES USUARIO DE MANGOS 3D - "OPCION NO" ----------------------------------------------
     else if ((question == "no") || (question == "No")) {
         alert("Es necesario crear un usuario.")
